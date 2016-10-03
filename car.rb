56b26303efdd157e38d530aa1dad5b72d39cd289
Alex Bazlinton <abazlinton@gmail.com>
2016-10-03 20:15:11 +0100

    engine now handles speed...hopefully :P

--- car.rb
+++ car.rb
@@ -1,14 +1,30 @@
+require_relative 'engine'
+
+
 class Car
 
 	attr_reader :model, :top_speed, :engine, :speed, :fuel	
 
-	def initialize(model, top_speed, engine = "standard")
+	def initialize(model, top_speed, engine)
 		@model = model
 		@top_speed = top_speed
-		@engine = engine
-		@fuel = 100
+		@engine = Engine.new(engine)
+		@fuel = 100 ##### CONSTANT #####
 		@speed = 0
 	end
+
+	def accelerate
+		@speed += @engine.accelerate
+		@fuel -= 5
+	end
+
+	def break
+		@speed -= 10
+	end
+
+
+
+
 
 end
 
--- engine.rb
+++ engine.rb
@@ -1,16 +1,24 @@
 class Engine
 
-
-	attr_reader :acc_multiplier, :fuel_multiplier
+	attr_reader :acc, :fuel_use, :type
 
 	def initialize(type)
-		if type == "turbo"
-			@acc_multiplier = 1.2
-			@fuel_multiplier = 1.3
-		elsif type == "eco"
-			@acc_multiplier = 0.8
-			@fuel_multiplier = 0.7
+		@type = type
+		case @type
+		when "turbo"
+			@acc = 12
+			@fuel_use = 7
+		when "eco"
+			@acc = 8
+			@fuel_use = 3
+		when "standard"
+			@acc = 10
+			@fuel_use = 5
 		end
+	end
+
+	def accelerate
+		return @acc
 	end
 
 
